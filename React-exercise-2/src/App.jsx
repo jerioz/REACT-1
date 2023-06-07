254c6245f6af9e21ec6784740b5ea88fa3299160
jerioz <escobedoranero@gmail.com>
2023-06-07 18:29:24 +0200

    add exercise 2

---
+++ React-exercise-2/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from '../components/Home/Home'
+import Heather from '../components/Heather/Heather'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Heather />
+      <Home />
+    </div>
+  )
+}
+
+export default App
